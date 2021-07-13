4f249426f4f2c089ddf81e2c3a2f0032a23df3f2
JuanMaGiraldo <juan.giraldo@kommit.co>
2021-07-13 16:42:29 -0500

    config

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :heroku_app, HerokuApp.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "admin",
+  password: "root",
   database: "heroku_app_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :heroku_app, HerokuApp.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "admin",
+  password: "root",
   database: "heroku_app_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
